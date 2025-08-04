2c749afd02f800610a2eca78da0feeb4e0e5e5b9
bharathpk <144653119+bharathpk@users.noreply.github.com>
2025-08-04 18:04:54 +0530

    Delete EventConsole directory

--- EventConsole/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using DAL.Model;
-using DAL.Repository;
-
-EventRepository eventRepository = new EventRepository();
-eventRepository.GetEvents();
